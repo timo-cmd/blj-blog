9e18a071c053c8de323d79e7cc2e40b7941f6c62
Damiano <info@damianoleo.ch>
2020-11-20 15:24:35 +0100

    push!

--- blogs.php
+++ blogs.php
@@ -1,13 +1,35 @@
-<?php include 'nav.php';?>
+<?php include 'nav.php';
+include 'initialize.php';
+
+$stmt = $pdo->prepare('SELECT * FROM `posts` WHERE id = :id');
+$stmt->execute([':id' => 1]);
+
+foreach($stmt->fetchAll() as $x) {
+    var_dump($x);
+    var_dump($_POST);
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="/blog/styles.css">
+    <link rel="stylesheet" href="/blj-blog/styles.css">
 </head>
 <body>
-    
+    <form action="/blj-blog/blogs.php" method="post">
+        <div class="name">
+            <label for="name">Name:</label><br>
+            <input type="text" id="name" name="name"><br>
+        </div><br>
+        
+        <div class="post">
+            <label for="name">Beitrag:</label><br>
+            <textarea name="post" rows="15" cols="60"></textarea> 
+        </div>
+        <input class="submit" type="submit" value="Submit">
+       
+    </form>
 </body>
 </html>
--- home.php
+++ home.php
@@ -1,13 +1,18 @@
-<?php include 'nav.php';?>
+<?php 
+include 'nav.php';
+include 'initialize.php'
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="/blog/styles.css">
+    <link rel="stylesheet" href="/blj-blog/styles.css">
 </head>
 <body>
-    
+    <h1>Welcome</h1>
 </body>
 </html>
