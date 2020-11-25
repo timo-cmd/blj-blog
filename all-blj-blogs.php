c76463c7781b78306cfeb5f9f62dbd08d7c286a3
Damiano <info@damianoleo.ch>
2020-11-25 16:29:13 +0100

    update

--- all-blj-blogs.php
+++ all-blj-blogs.php
@@ -1,5 +1,6 @@
 <?php
     include 'nav.php';
+    include 'initialize.php'
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -10,6 +11,17 @@
     <link rel="stylesheet" href="/blj-blog/styles.css">
 </head>
 <body>
-    
+    <?php
+
+    $sqlQuery = $dbConnection->query("SELECT * FROM `blog_url`");
+    $urls = $sqlQuery->fetchAll();
+    echo '<h1>Blogs meiner BLJ-Kollegen</h1>';
+
+    foreach($urls as $url) {
+        $link = '<a href="' . $url["blogUrl"] . '" target="_blank">' . $url["blogAuthor"] . '\'s Blog' . '</a>' . '<br>';
+
+    echo $link;
+    }
+    ?>
 </body>
 </html>
